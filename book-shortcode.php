e28565dc61393a4e7c879e1b830430aacaf56825
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 17:32:25 +0530

    comment added

--- book-shortcode.php
+++ book-shortcode.php
@@ -1,11 +1,15 @@
 <?php
-/*
-    ===============================
-    create shortcode
-    ===============================
-*/    
-add_shortcode('book', 'wpbook1_shortcode');
-function wpbook1_shortcode($attributes){
-    print_r($attributes);
-    return "this is dlfaldflasdd";   
+/**
+ * Create shortcode
+ */
+function WPbook_shortcode($attributes) 
+{
+    echo "<br/><h5> ID: " . $attributes['id'] . "</h5>";
+    echo "<br/><h5> Author name: " . $attributes['author_name'] . "</h5>";
+    echo "<br/><h5> Year: " . $attributes['year'] . "</h5>";
+    echo "<br/><h5> Category: " . $attributes['category'] . "</h5>";
+    echo "<br/><h5> Tag: " . $attributes['tag'] . "</h5>";
+    echo "<br/><h5> Publisher:" . $attributes['publisher'] . "</h5><br/><br/>";
+    //echo "<br/><h5> price: </h5>" . $attributes['price'] . "<br/><br/>";
 }
+add_shortcode('book', 'WPbook_shortcode');
